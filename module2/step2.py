64824c0d1787028d2503f6fe24c8ccf7bd49c232
tw <12754678004@qq.com>
2024-10-21 22:49:33 +0800

    upload 1st

--- module2/step2.py
+++ module2/step2.py
@@ -1,29 +1,30 @@
 from djitellopy import Tello
 import yaml
 def process_movements(tello,param):
-    with open('config.yaml', 'r') as file:
+    with open('module2/config.yaml', 'r') as file:
         config = yaml.safe_load(file)
 
     step2_config = config.get(param, {})
-    for direction, distance in step2_config.items():
+    for distance, direction in step2_config.items():
         if direction == "rotate":
-            tello.rotate_clockwise(distance)
+            print("direction is :" + direction + "  distance: " + distance)
+            # tello.rotate_clockwise(distance)
         else:
             tello.move(direction, distance)
-        print("the height is :" + tello.get_height())
+            print("direction is :" + direction + "  distance: " + distance)
 
 def flyStep2(tello):
-    with open('config.yaml', 'r') as file:
+    with open('module2/config.yaml', 'r') as file:
         config = yaml.safe_load(file)
         # keep live
-        tello.send_keepalive()
+        # tello.send_keepalive()
 
         # tello.go_xyz_speed_mid(**config['x']x, y, z, speed, mid)
         #x	int	-500-500	必填
         # y	int	-500-500	必填
         # z	int	-500-500	必填
         # speed	int	10-100	必填
-        tello.go_xyz_speed(**config['go_xyz_speed_mid'])
+        # tello.go_xyz_speed(**config['go_xyz_speed_mid'])
 
         # param1:up, down, left, right, forward or back / param2 :20-500 cm
         process_movements(tello,"step2")
